3981cf7ab7b302a6c8e147cbffb2a1a2f6b8af5a
BlaiseGratton <blaisegratton@gmail.com>
2017-08-19 18:32:44 -0500

    add fake data and render in list view

--- app/listController.js
+++ app/listController.js
@@ -1,8 +1,12 @@
 ;(function () {
-  const ListController = function () {
+  const ListController = function ($http, $timeout) {
     const vm = this
     
     vm.message = "This is the list of TILs"
+
+    $http.get('/fakeData.json').then(response => {
+      vm.tils = response.data
+    })
   }
 
   angular.module('til-app').controller('ListController', ListController)
