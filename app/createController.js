348e65062abcdf68c5658158c5eb3d4c02e188dc
BlaiseGratton <blaisegratton@gmail.com>
2017-08-19 20:55:34 -0500

    stub out method for posting TIL form

--- app/createController.js
+++ app/createController.js
@@ -1,9 +1,21 @@
 ;(function () {
-  const CreateController = function () {
+  const CreateController = function ($http, $location) {
     const vm = this
 
-    
-    vm.message = "This is the create TIL view"
+    vm.submit = () => {
+      const postData = {
+        text: vm.tilText,
+        userId: ''
+      }
+      $location.path('/list')
+      /*
+      $http.post('', postData).then(success => {
+        $location.path('/list')
+      }, error => {
+        // handle error
+      })
+      */
+    }  
   }
 
   angular.module('til-app').controller('CreateController', CreateController)
